82caafbd5654bfb4b9563b59a0da3eb59c5d5721
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-08-05 19:57:10 +0330

    S

--- pos_app/main.py
+++ pos_app/main.py
@@ -1,16 +1,34 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLineEdit, QFormLayout
 import sys
+from PySide6.QtCore import Qt
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setMinimumSize(480, 320)
-        label = QLabel("به اولین برنماه پای ساید 6 خوش آمدید")
-        self.setCentralWidget(label)
+        form = QFormLayout()
+        self.username = QLineEdit()
+        self.username.setPlaceholderText("نام کاربری")
+        form.addRow("نام کاربری", self.username)
+        self.password = QLineEdit()
+        self.password.setPlaceholderText("کلمه عبور")
+        form.addRow("کلمه عبور", self.password)
+        button = QPushButton("ورود")
+        form.addRow(button)
+        button.clicked.connect(self.on_click)
+        widget = QWidget()
+        widget.setLayout(form)
+        self.setCentralWidget(widget)
+
+    def on_click(self):
+        username = self.username.text()
+        password = self.password.text()
+        print(f"hello {username} {password}")
 
 
 app = QApplication(sys.argv)
+app.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
 window = MainWindow()
 window.show()
 sys.exit(app.exec())
